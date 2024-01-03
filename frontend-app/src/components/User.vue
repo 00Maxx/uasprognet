13284f8c802b84e70e03a26b00bc2fbf73a76ee7
Max <paendong.2205551039@student.unud.ac.id>
2024-01-04 02:05:31 +0800

    edit1

--- frontend-app/src/components/User.vue
+++ frontend-app/src/components/User.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <h2>Form Nasabah</h2>
+        <h2>Form Vue</h2>
         <form @submit.prevent="simpan()">
             <div class="mb-3 form-group">
                 <label>Nama</label>
